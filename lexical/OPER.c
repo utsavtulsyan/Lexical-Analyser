46ab57a56cd75a8a4c28fde7aca1c08d2c640d52
utsavtulsyan <utsavtulsyan44@gmail.com>
2018-03-27 19:49:01 +0530

    Lexical-analyser

---
+++ lexical/OPER.c
@@ -0,0 +1,26 @@
+(	openpara
+)	closepara
+{	openbrace
+}	closebrace
+<  	lesser
+>	greater
+"	doublequote
+'	singlequote
+:	colon
+;	semicolon
+#	preprocessor
+=	equal
+<=  less_equal
+>=  greater_equal
+!=  not_equal_to
+==	assign
+%	percentage
+^	bitwise
+&	reference
+*	star
++	add
+-	sub
+\	backslash
+/	slash
+
+
---
+++ lexical/inter.c
@@ -0,0 +1,1 @@
+	#	include	<	stdio.h	>		$	void	 	main	(		)		$		{		$	int	 	a	=	5	;		$	printf	(		"		%	d	"		,	a	)		;		$		}	
